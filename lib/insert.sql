b2206c33235fdff11ba5a529db62547b98b57dbc
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-07-17 13:25:58 -0400

    Add bears to table

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+  ('Mr. Chocolate', 8, 'M', 'black', 'cool', 1),
+  ('Rowdy', 7, 'M', 'brown', 'angry', 0),
+  ('Tabitha', 6, 'F', 'white', 'cool', 1),
+  ('Sergeant Brown', 5, 'M', 'brown', 'nice', 0),
+  ('Melissa', 4, 'F', 'black', 'aggressive', 1),
+  ('Grinch', 3, 'M', 'brown', 'cool', 0),
+  ('Wendy', 2, 'F', 'black', 'cool', 1),
+  ( NULL, 1, 'M', 'white', 'cool', 1);
